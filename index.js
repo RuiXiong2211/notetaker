85f3ac1c4011af938d7b96815a7d45c460faf909
DESKTOP-4RG4IVB\ruixi <ruixiong8@hotmail.com>
2022-09-10 16:30:40 +0800

    cicd

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.use(express.json())
 const noteRouter = require('./routes/note')
 app.use('/note', noteRouter)
 // Setup server port
-let port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express@'));
 // Launch app to listen to specified port
-app.listen(port, function () {
+app.listen(PORT, function () {
      console.log("Running backend on port " + port);
 });
 
