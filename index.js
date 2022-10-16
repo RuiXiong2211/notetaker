70cfca6c64b997a797b250486d9220b2136e8fa9
DESKTOP-4RG4IVB\ruixi <ruixiong8@hotmail.com>
2022-10-16 16:32:36 +0800

    update mongo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 //mongoose.connect("mongodb://127.0.0.1/notes")
 mongoose
   .connect(
-    process.env.LOCAL_DATABASE_URL,
+    "mongodb://localhost/notes",
     {
      useNewUrlParser: true,
    }
